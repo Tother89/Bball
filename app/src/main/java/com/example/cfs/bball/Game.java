035883c72a99c733ed57e104a47d4b886260bcef
Dallin Toth <toth.dallin@gmail.com>
2017-03-08 18:12:07 -0700

    First commit

---
+++ app/src/main/java/com/example/cfs/bball/Game.java
@@ -0,0 +1,27 @@
+package com.example.cfs.bball;
+
+import java.sql.Time;
+import java.util.ArrayList;
+
+/**
+ * Created by cfs on 2/15/2017.
+ */
+
+public class Game {
+    private int score;
+    private Time time;
+
+
+    private int quarter = 12;
+    private Team team1;
+    private Team team2;
+
+
+    public Game(){
+
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/example/cfs/bball/Team.java
@@ -0,0 +1,8 @@
+package com.example.cfs.bball;
+
+/**
+ * Created by cfs on 2/15/2017.
+ */
+
+public class Team {
+}
